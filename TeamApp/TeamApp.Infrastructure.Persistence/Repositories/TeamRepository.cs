fb44e9280fb9bc4499407d80f5bf1318268a761b
Dung Nguyen <dunghkuit@gmai.com>
2021-05-04 22:09:41 +0700

    migration db online

--- TeamApp/TeamApp.Infrastructure.Persistence/Repositories/TeamRepository.cs
+++ TeamApp/TeamApp.Infrastructure.Persistence/Repositories/TeamRepository.cs
@@ -75,6 +75,8 @@ namespace TeamApp.Infrastructure.Persistence.Repositories
                     GroupChatUserIsDeleted = false,
                 });
 
+                var leader = await _dbContext.User.FindAsync(teamReq.TeamLeaderId);
+
                 return new TeamResponse
                 {
                     TeamId = entity.TeamId,
@@ -83,6 +85,10 @@ namespace TeamApp.Infrastructure.Persistence.Repositories
                     TeamDescription = entity.TeamDescription,
                     TeamCreatedAt = entity.TeamCreatedAt,
                     TeamCode = entity.TeamCode,
+                    TeamLeaderName = leader.FullName,
+                    TeamLeaderImageUrl = leader.ImageUrl,
+                    TeamMemberCount = 1,
+                    TeamImageUrl = "https://scontent.fsgn5-3.fna.fbcdn.net/v/t1.6435-9/95384801_3541411182540556_323501399205740544_n.png?_nc_cat=1&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=PNRMG3JZivEAX8fDiPY&_nc_ht=scontent.fsgn5-3.fna&oh=f9d490f5d7f7a1b81999da2845b80923&oe=609FA0C7",
                 };
             }
             return null;
