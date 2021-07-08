821e54c2e5124e1a015d115e62f7213a0515e2be
Dung Nguyen <dunghkuit@gmai.com>
2021-07-08 19:54:42 +0700

    refactor model

--- TeamApp/TeamApp.Application/DTOs/AppSearch/GroupChatSearchResponse.cs
+++ TeamApp/TeamApp.Application/DTOs/AppSearch/GroupChatSearchResponse.cs
@@ -4,10 +4,19 @@ using System.Text;
 
 namespace TeamApp.Application.DTOs.AppSearch
 {
+    public class MemberChat
+    {
+        public string Id { get; set; }
+        public string MemberName { get; set; }
+        public string MemberAvatar { get; set; }
+    }
     public class GroupChatSearchResponse
     {
-        public string GroupChatId { get; set; }
-        public string GroupChatName { get; set; }
+        public string ChatId { get; set; }
+        public string ChatName { get; set; }
+        public string ChatImage { get; set; }
         public string Link { get; set; }
+        public bool? ChatIsOfTeam { get; set; }
+        public List<MemberChat> ChatListMembers { get; set; }
     }
 }
